e59aa337709c04719c9b9b0fe3f842edcee82591
Abbe Keultjes <flauwekeul@gmail.com>
2018-10-18 07:44:48 +0200

    add add() and subtract() methods to Point

--- src/point.ts
+++ src/point.ts
@@ -3,5 +3,13 @@ export class Point {
         return point1.x === point2.x && point1.y === point2.y
     }
 
+    static add(point1: Point, point2: Point) {
+        return new this(point1.x + point2.x, point1.y + point2.y)
+    }
+
+    static subtract(point1: Point, point2: Point) {
+        return new this(point1.x - point2.x, point1.y - point2.y)
+    }
+
     constructor(public x: number, public y: number) { }
 }
